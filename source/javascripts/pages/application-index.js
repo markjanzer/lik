6226ff9b1bdef10ebd0f3f9f473220cabc4d44ed
markjanzer <mark.janzer@gmail.com>
2019-05-22 15:49:34 -0700

    Finish rough draft of hero scroll + transition.

--- source/javascripts/pages/application-index.js
+++ source/javascripts/pages/application-index.js
@@ -1,17 +1,16 @@
-// Hero
-// REDO THIS WITH WAYPOINTS
-var heroHeight = $(".hero")[0].offsetHeight;
-function setNavOpacity() {
+// -------- Scroll Functions --------
+
+// Opacity for navbar
+function setNavOpacity(heroHeight) {
   if (window.pageYOffset > heroHeight) {
     $(".navbar")[0].classList.add("navbar--translucent");
   } else {
     $(".navbar")[0].classList.remove("navbar--translucent");
   }
 }
-setNavOpacity();
-window.addEventListener('scroll', setNavOpacity);
 
-function transformKeyboard() {
+// Transform keyboard from TK to LIK
+function transformKeyboard(scrollLength) {
   var scrollLength = $(".hero__text--second")[0].offsetTop;
   var white = { red: 255, green: 255, blue: 255, alpha: 1 }
   var gray = { red: 151, green: 151, blue: 151, alpha: 1 }
@@ -40,10 +39,42 @@ function transformKeyboard() {
   })
 }
 
-transformKeyboard();
-window.addEventListener('scroll', transformKeyboard);
+function positionKeyboard(keyboardScrollEnd) {
+  // var scrollTop = window.pageYOffset;
+  var keyboard = $(".keyboard")[0];
+  // Maybe I should use calsses here instead
+  if (window.pageYOffset >= keyboardScrollEnd) {
+    keyboard.style.position = "absolute";
+    keyboard.style.top = "0px";
+  } else {
+    var top = $(".hero__text--first")[0].offsetHeight;
+    keyboard.style.position = "fixed";
+    keyboard.style.top = top + "px";
+  }
+}
 
+function animateHeroText(keyboardScrollEnd) {
+  var secondText = $(".hero__text--second")[0];
+  if (window.pageYOffset >= keyboardScrollEnd) {
+    secondText.classList.add("fadeInUp");
+  }
+}
 
+function onScroll() {
+  var heroHeight = $(".hero")[0].offsetHeight;
+  var keyboardScrollEnd = $(".hero__text--second")[0].offsetTop;
+
+  setNavOpacity(heroHeight);
+  transformKeyboard(keyboardScrollEnd);
+  positionKeyboard(keyboardScrollEnd);
+  animateHeroText(keyboardScrollEnd);
+}
+
+// -------- Helpers --------
+
+function numberTransition(n1, n2, percent) {
+  return n1 + ((n2 - n1) * percent);
+}
 
 function transformColor(startingColor, endingColor, percent) {
   var newColor = {
@@ -55,10 +86,24 @@ function transformColor(startingColor, endingColor, percent) {
   return `rgb(${newColor.red}, ${newColor.green}, ${newColor.blue}, ${newColor.alpha})`;
 }
 
-function numberTransition(n1, n2, percent) {
-  return n1 + ((n2 - n1) * percent);
-}
+// function scrollDirection() {
+//   var scrollTop = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+//   var direction;
+//   if (scrollTop > lastScrollTop) {
+//     direction = "down";
+//   } else {
+//     direction = "up";
+//   }
+//   lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
+//   return direction;
+// }
 
+
+// -------- Onload ---------
+
+// var lastScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+onScroll();
+window.addEventListener('scroll', onScroll);
 
 // Force page scroll position to the top on page load
 window.onbeforeunload = function () {
@@ -72,18 +117,6 @@ if (window.pageYOffset == 0) {
     zenscroll.toY(scrollHeight, 1400) 
   }, 1500)
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 // Chart
 new Chartist.Line('.ct-chart', data, basicOptions);
